263c8132d4829781b585ce6ec65b9f3a1a8052f6
Arashs6 <arash.sardari6@gmail.com>
2021-09-05 15:24:16 +0430

    init

---
+++ src/app/directive/if/if-directive.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-if-directive',
+  templateUrl: './if-directive.component.html',
+  })
+
+  export class IfComponent {
+  public courses: Array<number> = [2,4]
+}
